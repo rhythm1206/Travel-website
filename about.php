fa35480a6b461ed14588e1f1d48ca43fce5ed65b
rhythm1206 <64724631+rhythm1206@users.noreply.github.com>
2020-08-23 20:43:50 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Travel</title>
+	<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  
+</head>
+<body>
+
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">RHYTHM TRAVELS</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Services</a>
+      </li>
+       <li class="nav-item">
+        <a class="nav-link" href="about.php">About</a>
+      </li>
+       <li class="nav-item">
+        <a class="nav-link" href="#">Contact</a>
+      </li>
+       
+     
+      
+      
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+<div class="jumbotron">
+  <h1>Rhythm tourism</h1>
+  <p>We provide the whole package for your travel and tourism.</p>
+</div>
+<section class="my-5">
+  <div class="py-5">
+    <h2 class="text-center">About Us</h2>
+  </div>
+  <div class="container-fluid">
+  <div class ="row">
+    <div class="col-lg-6 col-md-6 col-12">
+      <img src="image/image5.jpg" class="img-fluid aboutimg ">
+    </div>
+    <div class="col-lg-6 col-md-6 col-12">
+      <h3 class="display-4">Rhythm Travels</h3>
+      <p class="py-3">Welcome to a soulful experience of vacationing in India, the cradle of ancient civilization with rich cultural heritage. Experience the sights and sounds of its amazing diversity that is embedded in its geography, people and their cultures. </p>
+      <a href="about.php" class="btn btn-success">Know more</a>
+    </div>
+  </div>
+</div>
+
+</section>
+
+<footer>
+    <p class="p-3 bg-dark text-white text-center">@rhythmnegi</p>
+  </footer>
+
+
+
+
+
+</body>
---
+++ index.php
@@ -0,0 +1,226 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Travel</title>
+	<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  
+</head>
+<body>
+
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">RHYTHM TRAVELS</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Services</a>
+      </li>
+       <li class="nav-item">
+        <a class="nav-link" href="about.php">About</a>
+      </li>
+       <li class="nav-item">
+        <a class="nav-link" href="#">Contact</a>
+      </li>
+       
+     
+      
+      
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+
+<div id="demo" class="carousel slide" data-ride="carousel">
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+  </ul>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="image/image5.jpg" alt="Los Angeles" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>Los Angeles</h3>
+        <p>We had such a great time in LA!</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="image/image2.jpg" alt="Chicago" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>Chicago</h3>
+        <p>Thank you, Chicago!</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="image/new2.jpg" alt="New York" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>New York</h3>
+        <p>We love the Big Apple!</p>
+      </div>   
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
+<section class="my-5">
+	<div class="py-5">
+		<h2 class="text-center">About Us</h2>
+	</div>
+	<div class="container-fluid">
+	<div class ="row">
+		<div class="col-lg-6 col-md-6 col-12">
+			<img src="image/image5.jpg" class="img-fluid aboutimg ">
+		</div>
+		<div class="col-lg-6 col-md-6 col-12">
+			<h3 class="display-4">Rhythm Travels</h3>
+			<p class="py-3">Welcome to a soulful experience of vacationing in India, the cradle of ancient civilization with rich cultural heritage. Experience the sights and sounds of its amazing diversity that is embedded in its geography, people and their cultures. </p>
+			<a href="about.php" class="btn btn-success">Know more</a>
+		</div>
+	</div>
+</div>
+
+</section>
+
+<section class="my-5">
+	<div class="py-5">
+		<h2 class="text-center">Our Services</h2>
+	</div>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-lg-4 col-md-4 col-12">
+				<div class="card">
+				
+   				<img class="card-img-top" src="image/new3.jpg" alt="Card image">
+  					<div class="card-body">
+    				<h4 class="card-title">Venice </h4>
+    				<p class="card-text">The beautiful city</p>
+    				<a href="#" class="btn btn-primary">See Profile</a>
+  				</div>
+			</div>
+		</div>
+
+		<div class="col-lg-4 col-md-4 col-12">
+				<div class="card">
+				
+   				<img class="card-img-top" src="image/new4.jpg" alt="Card image">
+  					<div class="card-body">
+    				<h4 class="card-title">Bali</h4>
+    				<p class="card-text">Visit the beautiful beaches in the world.</p>
+    				<a href="#" class="btn btn-primary">See Profile</a>
+  				</div>
+			</div>
+		</div>
+
+		<div class="col-lg-4 col-md-4 col-12">
+				<div class="card">
+				
+   				<img class="card-img-top" src="image/new5.jpg" alt="Card image">
+  					<div class="card-body">
+    				<h4 class="card-title">California </h4>
+    				<p class="card-text">Some beautiful trek.</p>
+    				<a href="#" class="btn btn-primary">See Profile</a>
+  				</div>
+			</div>
+		</div>
+	</div>
+ </section>
+
+
+ <section class="my-5">
+	<div class="py-5">
+		<h2 class="text-center">Gallery</h2>
+	</div>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-lg-4 col-md-4 col-12">
+				<img src="image/new1.jpg" class="img-fluid pb-4">
+			</div>
+			<div class="col-lg-4 col-md-4 col-12">
+				<img src="image/new6.jpg" class="img-fluid pb-4">
+			</div>
+			<div class="col-lg-4 col-md-4 col-12">
+				<img src="image/new7.jpg" class="img-fluid pb-4">
+			</div>
+			<div class="col-lg-4 col-md-4 col-12">
+				<img src="image/new8.jpg" class="img-fluid pb-4">
+			</div>
+			<div class="col-lg-4 col-md-4 col-12">
+				<img src="image/new9.jpg" class="img-fluid pb-4">
+			</div>
+			<div class="col-lg-4 col-md-4 col-12">
+				<img src="image/new10.jpg" class="img-fluid pb-4">
+			</div>
+		</div>
+
+		
+	</div>
+
+
+</section>
+
+ <section class="my-5">
+	<div class="py-5">
+		<h2 class="text-center">Contact</h2>
+	</div>
+		<div class="w-50 m-auto">
+			<form action="userinfo.php" method="post">
+				<div class="form-group">
+					<label>Username</label>
+					<input type="text" name="user" autocomplete="off class="form-control">
+				</div>
+					<div class="form-group">
+					<label>Email id</label>
+					<input type="text" name="email" autocomplete="off class="form-control">
+				</div>
+					
+					<div class="form-group">
+					<label>Mobile</label>
+					<input type="text" name="mobile" autocomplete="off class="form-control">
+					</div>
+					<div class="form-group">
+					<label>Comment</label>
+					<textarea class="form-control" name="comment">
+						
+					</textarea>
+				</div>
+				 <button type="submit" class="btn btn-success">Submit</button>
+			</form>
+		</div>
+
+
+	</section>
+	
+	<footer>
+		<p class="p-3 bg-dark text-white text-center">@rhythmnegi</p>
+	</footer>
+
+
+
+
+
+
+
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+</body>
+</html>
